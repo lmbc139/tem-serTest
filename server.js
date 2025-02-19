9387278091021bfdcb4b274cbfd43e410cc12614
Lmbc <c@CX>
2025-02-19 19:52:49 +0800

    增加save界面，但未解决时间不统一问题

--- server.js
+++ server.js
@@ -17,7 +17,17 @@ app.use(bodyParser.json());
 // 从 MySQL 获取温度数据
 async function getTemperatureData() {
   try {
-    const [rows] = await pool.query('SELECT time, temperature FROM temperature_logs');
+    const [rows] = await pool.query('SELECT time, temperature FROM temperature_logs ORDER BY time');
+    return rows;
+  } catch (error) {
+    console.error('Error fetching data from MySQL:', error);
+    return [];
+  }
+}
+
+async function getSavedData() {
+  try {
+    const [rows] = await pool.query('SELECT time, temperature1, temperature2, temperature3, temperature4, temperature5, temperature6,mean_temperature FROM temperature_save ORDER BY time DESC');
     return rows;
   } catch (error) {
     console.error('Error fetching data from MySQL:', error);
@@ -32,6 +42,16 @@ app.get('/', async (req, res) => {
     res.render('index', { data: data });
   } catch (error) {
     console.error('Error rendering index page:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/save', async (req, res) => {
+  try {
+    const data = await getSavedData();
+    res.render('save', { data: data });
+  } catch (error) {
+    console.error('Error rendering save page:', error);
     res.status(500).send('Internal Server Error');
   }
 });
@@ -52,7 +72,23 @@ app.post('/add-temperature', async (req, res) => {
   }
 });
 
+app.post('/add-save', async (req, res) => {
+  try {
+    const { time, mean_temperature , temperature1, temperature2, temperature3, temperature4, temperature5, temperature6 } = req.body;
+    
+    if (!time || !mean_temperature|| !temperature1 || !temperature2 || !temperature3 || !temperature4 || !temperature5 || !temperature6) {
+      return res.status(400).send('Time and temperature are required');
+    }
+
+    const [result] = await pool.query('INSERT INTO temperature_save (temperature1, temperature2, temperature3, temperature4, temperature5, temperature6, mean_temperature ,time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [temperature1, temperature2, temperature3, temperature4, temperature5, temperature6, mean_temperature ,time]);
+    res.status(201).send('Temperature save added successfully');
+  } catch (error) {
+    console.error('Error adding temperature save:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 // 启动服务器
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
